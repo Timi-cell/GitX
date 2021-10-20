6a78928b0aadc4e7e25a81b90b5a88274e070732
Timi-cell <adedayotimilehin10@gmail.com>
2021-10-20 16:45:43 +0100

    finished project GitX successfully

--- src/App.js
+++ src/App.js
@@ -1,29 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/header";
+import Input from "./components/input";
+import Body from "./components/body";
+import { Container } from "react-bootstrap";
 
+const GITHUB_API = "https://api.github.com/users";
 class App extends Component {
-  render() {
-      return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-  }
+  state = {
+    name: "---",
+    username: "",
+    picture: "./images/github-dark.png",
+    date_joined: "---",
+    bio: "---",
+    repos: 0,
+    followers: 0,
+    following: 0,
+    location: "---",
+    twitter_username: "---",
+    blog: "---",
+    account_address: "",
+  };
+  connectGithub(username) {
+    fetch(`${GITHUB_API}/${username}`, { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        const {
+          name,
+          login,
+          avatar_url,
+          created_at,
+          bio,
+          public_repos,
+          followers,
+          following,
+          location,
+          twitter_username,
+          blog,
+          html_url,
+        } = data;
 
-};
+        const date = new Date(created_at);
+
+        this.setState({
+          name,
+          username: login,
+          picture: avatar_url,
+          date_joined: date.toLocaleString(),
+          bio,
+          repos: public_repos,
+          followers,
+          following,
+          location,
+          twitter_username,
+          blog,
+          account_address: html_url,
+        });
+      });
+  }
+  handleSubmit = () => {
+    // connect to Github
+    this.connectGithub(this.state.username);
+  };
+
+  handleInput = ({ value }) => {
+    const username = value;
+    this.setState({ username });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container className="d-flex flex-column justify-content-center align-items-center">
+          <Input
+            onSubmit={this.handleSubmit}
+            handleInput={this.handleInput}
+            username={this.state.username}
+          />
+          <Body data={this.state} />
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default App;
